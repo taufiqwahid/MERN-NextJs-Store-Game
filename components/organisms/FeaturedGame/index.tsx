911c9193b7521aa8ea961584e690c97d31e68c5b
taufiqwahid <taufiq.wahid58@gmail.com>
2022-05-04 05:34:48 +0800

    featured game

---
+++ components/organisms/FeaturedGame/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import GameItem from "../../molecules/GameItem";
+
+export default function FeaturedGame() {
+  return (
+    <section className="featured-game pt-50 pb-50">
+      <div className="container-fluid">
+        <h2 className="text-4xl fw-bold color-palette-1 mb-30">
+          Our Featured
+          <br /> Games This Year
+        </h2>
+        <div
+          className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
+          data-aos="fade-up"
+        >
+          <GameItem
+            src="/img/Thumbnail-1.png"
+            title="Super Mechs"
+            device="Mobile"
+          />
+          <GameItem
+            src="/img/Thumbnail-2.png"
+            title="Call of Duty: Modern"
+            device="Mobile"
+          />
+          <GameItem
+            src="/img/Thumbnail-3.png"
+            title="Mobile Legends"
+            device="Mobile"
+          />
+          <GameItem
+            src="/img/Thumbnail-4.png"
+            title="Clash of Clans"
+            device="Mobile"
+          />
+          <GameItem
+            src="/img/Thumbnail-5.png"
+            title="Valorant"
+            device="Desktop"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
